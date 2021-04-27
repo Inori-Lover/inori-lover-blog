03c97e7fc964033240938f530e77688a8cdd4628
inori-lover <337938188@qq.com>
2021-04-27 23:14:24 +0800

    feat: 修正Pool api设计，all等功能等普通的Promise库完成

--- src/pages/2021-04-18/pool.ts
+++ src/pages/2021-04-18/pool.ts
@@ -1,45 +1,40 @@
 class Pool {
   /** 待处理 */
-  private pendding = [];
+  private pendding: (() => Promise<unknown>)[] = []
   /** 资源池 */
-  private pool = [];
+  private pool: (() => Promise<unknown>)[] = []
 
-  constructor(/** 资源池深度 */ private depth: number) {
+  constructor(/** 资源池深度 */ private depth: number = 1) {
     //
   }
 
   /** 补全全部资源池空位（可能情况下） */
-  private run = () => {
+  private run = (): void => {
     //
-  };
+  }
 
   /** 压入任务 */
-  public add = () => {
-    //
-  };
-
-  /** 类似Promise.all行为，对象是当前时间点的pool（不包括之后压入的） */
-  public all = () => {
-    //
-  };
+  public add = <T = unknown>(task: () => Promise<T>): Promise<T> => {
+    return {} as any
+  }
 
   /** 获取当前等待队列长度 */
-  public get pendingLen () {
+  public get pendingLen() {
     return this.pendding.length
   }
 
   /** 获取当前设置的资源池深度 */
-  public get poolDepth () {
+  public get bufferLen() {
     return this.depth
   }
 
   /** 获取当前设置的资源池深度，实时，可能与设置值不一致（动态扩容、缩容的情况） */
-  public get currentPoolDepth () {
+  public get poolDepth() {
     return this.pool.length
   }
 
-  /** 修改资源池深度 */
-  public changeBuffenLen = (buffer: number) => {
-    this.depth = buffer;
-  };
+  /** 修改资源池深度 @param bufferLen 深度，必须大于等于1否则抛错 */
+  public changeBuffenLen = (bufferLen: number) => {
+    this.depth = bufferLen
+  }
 }
